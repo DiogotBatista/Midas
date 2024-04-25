39ecaeacb92e90f7bd6adb4399f0cdf9ca76b63b
Diogo Batista <diogotadeub@gmail.com>
2024-04-25 11:14:36 -0300

    mudanças

--- Midas/urls.py
+++ Midas/urls.py
@@ -16,6 +16,5 @@ urlpatterns = [
     path('despesas/', include('despesas.urls')),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
-    path('dashboard/', include('dashboard.urls')),
 
 ]
--- dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-
-
-urlpatterns = [
-
-    path('plotly_dash/', include('django_plotly_dash.urls')),
-]
